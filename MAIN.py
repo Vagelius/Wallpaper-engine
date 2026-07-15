2a01f0d86d4533ad8aa0f751b7205037e0cb6403
Vagelius <bobvagelis.petrogiannis@gmail.com>
2026-07-15 14:15:00 +0300

    Add files via upload

--- MAIN.py
+++ MAIN.py
@@ -2,20 +2,31 @@ import ctypes
 import os
 
 
-def change_wallpaper(C:\Users\Vagelis Petrogiannis\Desktop\Misty-morning-in-a-dense-forest-sunlight-breaking-through-the-fog-wallpaper-Full-HD-1920x1080-1):
-           SPI_SETDESKWALLPAPER = 20
-           SPIF_UPDATEINFILE = 0x02
-           SPIF_SENDWININICHANGE =0x02
+def change_wallpaper(image_path):
+    SPI_SETDESKWALLPAPER = 20
 
-           try:
-           
-                ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, image_path,
-                                                   SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE)         
+    SPIF_UPDATEINIFILE = 0x01
+    SPIF_SENDWININICHANGE = 0x02
 
-                return True
-            except Exception as e:
-                print(f"Error changing wallpaper: {e}")
-                return False
+    try:
+        result = ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER,0,image_path,SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE,)
+
+        
+        return result != 0
+
+    except Exception as e:
+        print(f"Error changing wallpaper: {e}")
+        return False
 
 
-if __name__ == "__main__"
+if __name__ == "__main__":
+
+    
+    image_path = os.path.abspath(
+        r"C:\Users\Vagelis Petrogiannis\Desktop\Misty-morning-in-a-dense-forest-sunlight-breaking-through-the-fog-wallpaper-Full-HD-1920x1080-1.jpg"
+    )
+
+    if change_wallpaper(image_path):
+        print("Wallpaper changed successfully")
+    else:
+        print("Failed to change wallpaper")
